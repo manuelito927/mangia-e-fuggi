976c2b2c5bfd5ed10f0fceef9e75d7ec88845503
manuelito927 <filosofiaefficace@gmail.com>
2025-10-13 16:57:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ async function printOrder(orderId) {
     const table = db.data.tables.find(t => t.id === o.table_id);
 
     const lines = [];
-    lines.push('=== ${r?.name || 'RISTORANTE'} ===');
+    lines.push(`=== ${r?.name || 'RISTORANTE'} ===`);
     lines.push(ORDINE: ${o.code});
     lines.push(TAVOLO: ${table?.code || ''});
     lines.push(DATA: ${o.created_at});
