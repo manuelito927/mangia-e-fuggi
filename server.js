e702b15491d9b60457f69538aca871b57083bc31
manuelito927 <filosofiaefficace@gmail.com>
2025-10-13 14:24:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ import fs from 'fs'
 import { db, nowISO } from './db.js'
 
 dotenv.config()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname  = path.dirname(_filename);
 
 // ====== BASE ======
 const __filename = fileURLToPath(import.meta.url)
